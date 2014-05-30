eb31f27f0c86ab250f369b5b1a17694a35a49285
imizoguchi <ra103659@xaveco.lab.ic.unicamp.br>
2014-05-30 14:50:12 -0300

    Editing some html files

--- view/index.php
+++ view/index.php
@@ -6,5 +6,7 @@
 		<h1> MC536 SocialNET</h1>
 		<h2><a href="./person_new.php">Novo usu&aacute;rio</a></h2>
 		<h2><a href="./person_list.php">Listar usu&aacute;rios</a></h2>
+		<h2><a href="./artist_list.php">Listar artistas</a></h2>
+		<h2><a href="./statistics.php">Estat&iacute;sticas</a></h2>
 	</body>
 </html>
---
+++ view/statistics.php
@@ -0,0 +1,42 @@
+<?php
+	$err = null;
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	
+	$con = mysql_connect("localhost:3306","mc536user07", "euzaraef") or die(mysql_error());
+	$selected = mysql_select_db("mc536db07", $con);
+
+	$orderby = "id";
+	if(isset($_GET['orderby']))
+		$orderby = $_GET['orderby'];
+
+   $sql = "SELECT * FROM MusicalArtist ORDER BY $orderby";
+   $result = mysql_query($sql,$con);
+?>
+<html>
+	<head>
+		<title> MC536 SocialNET - Estat&iacute;sticas</title>
+	</head>
+	<body>
+		<h1>Estat&iacute;sticas</h1>
+		<table cellpadding="6">
+		  <tr>
+		    <td><a href="?orderby=id">Id</a></td>
+		    <td><a href="?orderby=artistic_name">Name</a></td>
+		    <td><a href="?orderby=creation_date">Creation Date</a></td>
+		    <td><a href="?orderby=wiki_address">Wiki Address</a></td>
+		  </tr>
+		<?php
+			
+			while($row = mysql_fetch_array($result)) {
+				echo "<tr>";
+				echo "<td><a href='artist_update.php?id=".$row['id']."'>".$row['id']."</a></td>";
+				echo "<td><a href='artist_update.php?id=".$row['id']."'>".$row['artistic_name']."</td>";
+				echo "<td>".$row['creation_date']."</td>";
+				echo "<td><a href='".$row['wiki_address']."'>".$row['wiki_address']."</a></td>";
+				echo "</tr>";
+			}
+		?>
+		</table>
+	</body>
+</html>
