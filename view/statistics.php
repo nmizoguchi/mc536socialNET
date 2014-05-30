e22e59a19fc946567c0d33e3ebe0e663b06ea9e4
nickmm <nickmm@mit.edu>
2014-05-30 15:03:16 -0300

    Modifying statistics

--- view/statistics.php
+++ view/statistics.php
@@ -1,42 +1,35 @@
 <?php
-	$err = null;
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-	
-	$con = mysql_connect("localhost:3306","mc536user07", "euzaraef") or die(mysql_error());
-	$selected = mysql_select_db("mc536db07", $con);
+    $err = null;
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-	$orderby = "id";
-	if(isset($_GET['orderby']))
-		$orderby = $_GET['orderby'];
+    $con = mysql_connect("localhost:3306","mc536user07", "euzaraef") or die(mysql_error());
+    $selected = mysql_select_db("mc536db07", $con);
 
-   $sql = "SELECT * FROM MusicalArtist ORDER BY $orderby";
-   $result = mysql_query($sql,$con);
+    $sql = "select avg(rating), stddev(rating) from LikesMusic";
+    $result = mysql_query($sql,$con);
+    $rating_avg = mysql_fetch_array($result)['avg(rating)'];
+    $rating_stddev = mysql_fetch_array($result)['stddev(rating)'];
+
+    
+
 ?>
 <html>
-	<head>
-		<title> MC536 SocialNET - Estat&iacute;sticas</title>
-	</head>
-	<body>
-		<h1>Estat&iacute;sticas</h1>
-		<table cellpadding="6">
-		  <tr>
-		    <td><a href="?orderby=id">Id</a></td>
-		    <td><a href="?orderby=artistic_name">Name</a></td>
-		    <td><a href="?orderby=creation_date">Creation Date</a></td>
-		    <td><a href="?orderby=wiki_address">Wiki Address</a></td>
-		  </tr>
-		<?php
-			
-			while($row = mysql_fetch_array($result)) {
-				echo "<tr>";
-				echo "<td><a href='artist_update.php?id=".$row['id']."'>".$row['id']."</a></td>";
-				echo "<td><a href='artist_update.php?id=".$row['id']."'>".$row['artistic_name']."</td>";
-				echo "<td>".$row['creation_date']."</td>";
-				echo "<td><a href='".$row['wiki_address']."'>".$row['wiki_address']."</a></td>";
-				echo "</tr>";
-			}
-		?>
-		</table>
-	</body>
+    <head>
+        <title> MC536 SocialNET - Estat&iacute;sticas</title>
+    </head>
+    <body>
+        <h1>Estat&iacute;sticas</h1>
+        <h2>Ratings</h2>
+        <table cellpadding="6">
+            <tr>
+                <td>M&eacute;dia</td>
+                <td><?php echo $rating_avg ?></td>
+            </tr>
+            <tr>
+                <td>M&eacute;dia</td>
+                <td><?php echo $rating_stddev ?></td>
+          </tr>
+        </table>
+    </body>
 </html>
