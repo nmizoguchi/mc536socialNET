e9278c1edc9599df408d64f5759c559f371303f2
nickmm <nickmm@mit.edu>
2014-05-30 15:13:20 -0300

    Modifying statistics3

--- view/statistics.php
+++ view/statistics.php
@@ -12,8 +12,8 @@
     $rating_avg = $row['avg(rating)'];
     $rating_stddev = $row['stddev(rating)'];
 
-    $sql = "select LikesMusic.artist_id,MusicalArtist.artistic_name ,avg(LikesMusic.rating) from LikesMusic, MusicalArtist where LikesMusic.artist_id = MusicalArtist.id group by MusicalArtist.id order by avg(LikesMusic.rating) desc limit 20"    ;
-
+    // TOP 20 RATING MEDIO
+    $sql_top20_avg_rating = "select LikesMusic.artist_id,MusicalArtist.artistic_name ,avg(LikesMusic.rating) from LikesMusic, MusicalArtist where LikesMusic.artist_id = MusicalArtist.id group by MusicalArtist.id order by avg(LikesMusic.rating) desc limit 20";
 ?>
 <html>
     <head>
@@ -21,6 +21,7 @@
     </head>
     <body>
         <h1>Estat&iacute;sticas</h1>
+        
         <h2>Ratings</h2>
         <table cellpadding="6">
             <tr>
@@ -28,8 +29,28 @@
                 <td><?php echo $rating_avg ?></td>
             </tr>
             <tr>
-                <td>M&eacute;dia</td>
+                <td>Desvio padr&atilde;o</td>
                 <td><?php echo $rating_stddev ?></td>
+          </tr>
+        </table>
+        
+        <h2>Top 20 Artistas com maior rating m&eacute;dio</h2>
+        <table cellpadding="6">
+            <tr>
+                <td>Id</td>
+                <td>Nome</td>
+                <td>Rating m&eacute;dio</td>
+            </tr>
+            <tr>
+                <?php
+                    
+                    $result = mysql_query($sql_top20_avg_rating,$con);
+                    while($row = mysql_fetch_array($result)) {
+                        echo "<td>".$row['artist_id']."</td>";
+                        echo "<td>".$row['artistic_name']."</td>";
+                        echo "<td>".$row['avg(LikesMusic.rating)']."</td>";
+                    }
+                ?>
           </tr>
         </table>
     </body>
